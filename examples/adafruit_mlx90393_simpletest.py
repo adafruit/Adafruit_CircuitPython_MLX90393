e810fd939a2b4b248dd73ef5d4d0328def24f4e4
Kevin Townsend <contact@microbuilder.eu>
2019-01-17 22:40:53 +0100

    Fixed example to match API changes

--- examples/adafruit_mlx90393_simpletest.py
+++ examples/adafruit_mlx90393_simpletest.py
@@ -8,7 +8,7 @@ I2C_BUS = busio.I2C(board.SCL, board.SDA)
 SENSOR = adafruit_mlx90393.MLX90393(I2C_BUS, gain=adafruit_mlx90393.GAIN_1X)
 
 while True:
-    MX, MY, MZ = SENSOR.read_data(delay=1.0, raw=False)
+    MX, MY, MZ = SENSOR.read_data(raw=False)
     print("[{}]".format(time.monotonic()))
     print("X: {} uT".format(MX))
     print("Y: {} uT".format(MY))
@@ -16,3 +16,4 @@ while True:
     # Display the status field if an error occured, etc.
     if SENSOR.last_status > adafruit_mlx90393.STATUS_OK:
         SENSOR.display_status()
+    time.sleep(1.0)
